2afb9fe1af6a9da70fbfc0b34ae0ec4fdae4ef1f
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-08-20 15:50:15 -0300

    feat: concluindo desafio 1

--- DesafioGFT/Program.cs
+++ DesafioGFT/Program.cs
@@ -1,16 +1,37 @@
 ﻿var alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
-
+Console.WriteLine("Digite uma palavra que contenha uma sequência de pelo menos 3 letras em ordem alfabética");
 var palavra = Console.ReadLine();
 
+bool contem = false;
 string sequencia = string.Empty;
 
-for (int i = 0; i < palavra.Length; i++)
+if (palavra is not null && palavra.Length > 0)
 {
-    sequencia += palavra[i];
-
-    if (sequencia.Length >= 3)
+    for (int i = 0; i < palavra.Length; i++)
     {
-        alfabeto.Contains(sequencia);
+        sequencia += palavra[i];
+
+        if (sequencia.Length >= 3)
+        {
+            if (sequencia.Length > 3)
+            {
+                sequencia = sequencia.Substring(1, 3);
+            }
+
+            contem = alfabeto.Contains(sequencia);
+
+            if (contem) break;
+        }
+    }
+
+    if (contem)
+    {
+        Console.WriteLine($"A palavra {palavra} contém uma sequência em ordem alfabética, sequencia: {sequencia}");
+    }
+    else
+    {
+        Console.WriteLine($"A palavra {palavra} não contém uma sequência em ordem alfabética");
+
     }
 }
